bb94df23f90397734867b21fc5003ec6138b4026
Nina Leonidovna <abasheeva@gmail.com>
2025-10-04 20:46:36 +0200

    22 fibonacci

--- python/tasks/task_22_fibonacci.py
+++ python/tasks/task_22_fibonacci.py
@@ -4,4 +4,12 @@ def fibonacci(n: int) -> list[int]:
     """
     Vrátí prvních n členů Fibonacciho posloupnosti.
     """
-
+    f = []
+    if n > 0:
+        f.append(0)
+    if n > 1:
+        f.append(1)
+    if n > 2:
+        for i in range(2, n):
+            f.append(f[i-1] + f[i-2])
+    return f
