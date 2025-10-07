1631ec7365104a13f20febc91480f69749cd43e6
Nina Leonidovna <abasheeva@gmail.com>
2025-10-07 14:50:48 +0200

    40 Most frequent

--- python/tasks/task_40_most_frequent.py
+++ python/tasks/task_40_most_frequent.py
@@ -4,4 +4,23 @@ def most_frequent(items: list) -> any:
     """
     Vrátí nejčastěji se vyskytující prvek v seznamu.
     """
+    new_list = []
+    counter = []
+    for element in items:
+        if element in new_list:
+            n = new_list.index(element)
+            counter[n] = counter[n] + 1
+        else:
+            new_list.append(element)
+            counter.append(1)
+    n = maximum(counter)
+    return new_list[n]
 
+
+def maximum(counter):
+    max = 0
+    for i in counter:
+        if i > max:
+            max = i
+            number = counter.index(i)
+    return number
