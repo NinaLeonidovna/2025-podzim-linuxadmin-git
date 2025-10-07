47cfbfb266873955f9af916cb2b35ac3b8b9cdfb
Nina Leonidovna <abasheeva@gmail.com>
2025-10-07 14:29:17 +0200

    38 Matrix transponse

--- python/tasks/task_38_matrix_transpose.py
+++ python/tasks/task_38_matrix_transpose.py
@@ -4,4 +4,14 @@ def matrix_transpose(matrix: list[list[int]]) -> list[list[int]]:
     """
     Vrátí transponovanou matici.
     """
-
+    if matrix == []:
+        return ([])
+    n = len(matrix)
+    m = len(matrix[0])
+    new_matrix = []
+    for i in range(m):
+        element = []
+        for j in range(n):
+            element.append(matrix[j][i])
+        new_matrix.append(element)
+    return (new_matrix)
